fb99e102299595d55a2d6a47d20d42269c0466ef
Kevin Le Faucheur <lefaucheur.ke@gmail.com>
2023-05-04 00:20:54 +0200

    Provider store

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from "react-router-dom"; 
 import GlobalStyle from './styles/GlobalStyle';
 import { App } from './App';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <GlobalStyle />
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <GlobalStyle />
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
