4a84c2b9586c2a6d7e7228e259bb9cb95baeefb3
Kevin Le Faucheur <lefaucheur.ke@gmail.com>
2023-05-24 16:46:46 +0200

    Transaction and Modal isolating shared code

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Transaction } from './Transaction';
+import { 
+  Button, CollapseIcon, Column, Content, 
+  ContentHeader, ContentReveal, ContentRevealItem, 
+  ContentWrapper, DateResponsive, 
+  Day, Input, Month, NotesEdit, Select, Year 
+} from './Transaction.styled';
+import { dateFormat } from '../utils/dateFormat';
 
 const Background = styled.div`
   visibility: hidden;
-  background-color: rgba(0, 0, 0, 0.5);
+  background-color: rgba(0, 0, 0, 0.75);
   z-index: 10;
 
   position: fixed;
@@ -23,11 +29,14 @@ const Dialog = styled.div`
   left: 10%;
   top: 25%;
   width: 80%;
+  min-height: 200px;
+  border-radius: 5px;
+  padding-top: calc(30px + 2rem);
   /* background-color: rgba(#2c3e50, 1); */
   background-color: #FFF;
 `
 
-const Button = styled.button`
+const EyeButton = styled.button`
   position: absolute;
   right: 15px;
   top: 15px;
@@ -38,7 +47,14 @@ const Button = styled.button`
   border-radius: 5px;
 `
 
+const Separator = styled.div`
+  margin: 10px 0;
+  height: 2px;
+  background-color: #2c3e50;
+`
+
 export const Modal = ({ transaction}) => {
+  const [month, day, year] = dateFormat(transaction?.date);
 
   const close = () => {
     document.getElementById('modal').classList.remove('open');
@@ -47,12 +63,60 @@ export const Modal = ({ transaction}) => {
   return (
     <Background id='modal'>
       <Dialog>
-        <div id='modal-header'>
-          <p>Transaction:</p>
-          <Button onClick={close} className="fa fa-regular fa-eye"></Button>
-        </div>
+        <EyeButton onClick={close} className="fa fa-regular fa-eye"></EyeButton>
         <div id='modal-body'>
-          <Transaction transaction={transaction} /> 
+          <ContentWrapper>
+
+            <ContentHeader>
+
+              <Content>
+                <Column className='column-date'>{month + ' ' + day + ', ' + year}</Column>
+
+                <DateResponsive>
+                  <Month>{month}</Month>
+                  <Day>{day + 'th'}</Day>
+                  <Year>{year}</Year>
+                </DateResponsive>
+
+                <Column className='column-description'>{transaction?.description}</Column>
+                <Column className='column-amount'>${transaction?.amount}</Column>
+                <Column className='column-balance'>${transaction?.balance}</Column>
+              </Content>
+
+            </ContentHeader>
+
+            <Separator />
+
+            <ContentReveal>
+
+              {<ContentRevealItem>Transaction Type: {transaction?.infos?.transactionType}</ContentRevealItem>}
+
+              {<ContentRevealItem>Category:&nbsp;
+                {/* <>
+                  {selectValue}
+                  <i onClick={() => setIsCategoryEditing(!isCategoryEditing)} className="fa fa-solid fa-pencil" />
+                </>
+                <Select visibility={isCategoryEditing}  onChange={(e) => handleSetSelectValue(e.target.value)} name="categories">
+                  <option value="">--Please choose a category--</option>
+                  {categories.map(category => <option key={category} value={category}>{category}</option>)}
+                </Select> */}
+              </ContentRevealItem>}
+
+              {<ContentRevealItem>Notes:&nbsp;
+                {/* {noteInputValue}
+                <i onClick={() => setIsNotesEditing(!isNotesEditing)} className="fa fa-solid fa-pencil" />
+
+                <NotesEdit visibility={isNotesEditing}>
+                  <Input id={`transaction-note-${transaction.id}`} ref={inputRef} placeholder='Write something' />
+                  <Button onClick={() => handleSetNotesValue(inputRef.current.value)}>Save</Button>
+                  <Button onClick={() => setIsNotesEditing(!isNotesEditing)}>Cancel</Button>
+                </NotesEdit> */}
+
+              </ContentRevealItem>}
+
+            </ContentReveal>
+
+          </ContentWrapper>
         </div>
       </Dialog>
     </Background>
---
+++ src/components/Transaction.styled.jsx
@@ -0,0 +1,134 @@
+import styled from "styled-components"
+
+export const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: #fff;
+  width: 100%;
+  padding: 0.75rem 2rem 0.75rem 1rem;
+  margin: 2px 0;
+  font-weight: bold;
+
+  @media (max-width: 720px) {
+    padding: 0.5rem;
+  }
+`
+
+export const ContentHeader = styled.div`
+  display: flex;
+  margin-bottom: 0.5rem;
+  cursor: pointer;
+
+  @media (max-width: 720px) {
+    font-size: 1rem;
+  }
+`
+
+export const CollapseIcon = styled.div`
+  width: 20px;
+  margin-right: 1rem;
+
+  i {
+    cursor: pointer;
+    font-size: 1.5rem;
+    font-weight: 800;
+  }
+
+  @media (max-width: 720px) {
+    display: none;
+  }
+`
+
+export const Content = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+`
+
+export const DateResponsive = styled.div`
+  display: none;
+
+  @media (max-width: 720px) {
+    display: flex;
+    flex-direction: column;
+  }
+
+`
+
+export const Day = styled.div`
+
+`
+
+export const Month = styled.div`
+
+`
+
+export const Year = styled.div`
+
+`
+
+export const Column = styled.div`
+  &.column-date {
+    width: 15%;
+    
+    @media (max-width: 720px) {
+      display: none;
+    }
+  }
+  &.column-description {
+    width: 35%; 
+  }
+  &.column-amount {
+    width: 25%;    
+  }
+  &.column-balance {
+    width: 25%;    
+  }
+`
+
+export const ContentReveal = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding-left: calc(1rem + 20px);
+`
+
+export const ContentRevealItem = styled.div`
+  width: 100%;
+  margin: 0.5rem 0;
+  text-align: left;
+
+  i {
+    cursor: pointer;
+    width: 16px;
+    height: 16px;
+    margin: 0 1rem;
+  }
+`
+
+export const Select = styled.select`
+  visibility: ${(props => props?.visibility ? 'visible' : 'collapse')};
+`
+
+export const Button = styled.button`
+  cursor: pointer;
+  border-color: #00bc77;
+  background-color: #00bc77;
+  color: #fff;
+  font-weight: bold;
+  min-width: 100px;
+  padding: 2px;
+  margin-left: 0.25rem;
+`
+
+export const NotesEdit = styled.div`
+  display: inline;
+  visibility: ${(props => props?.visibility ? 'visible' : 'collapse')};
+`
+
+export const Input = styled.input`
+  font-weight: bold;
+  padding: 2px;
+  width: 30%;
+`
